0bffc6caf143f1412ebe30e95dc8f328bcf7a6b6
hoeyboey <hannahdwan96@gmail.com>
2017-05-23 21:13:17 +0100

    Full push
    A preliminary push to GitHub now that it works.

---
+++ controllers/default.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# -------------------------------------------------------------------------
+# This is a sample controller
+# - index is the default action of any application
+# - user is required for authentication and authorization
+# - download is for downloading files uploaded in the db (does streaming)
+# -------------------------------------------------------------------------
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    session.held_hand = []
+    return dict()
+    
+def first():
+    from calculations import acceptable_inputs
+    form = SQLFORM.factory(Field('card_input',
+                                 label='What is your next card?',
+                                 requires=IS_NOT_EMPTY()))
+    if form.process().accepted:
+        if form.vars.card_input in acceptable_inputs:
+            held_hand = session.held_hand.append(form.vars.card_input)
+    return dict(form=form)
+    
+def testing():
+    from calculations import play
+    session.full_deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 4
+    play(session)
+    return dict()
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
